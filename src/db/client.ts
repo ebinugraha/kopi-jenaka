e268d9e28ce82e9ac7c414c883adfb829de9e916
FebrianNugraha <febrian.14nugraha@gmail.com>
2025-12-23 14:55:36 +0700

    feat: 01 project setup

---
+++ src/db/client.ts
@@ -0,0 +1,11 @@
+// lib/prisma.ts
+import { PrismaClient } from "@/../prisma/generated/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
